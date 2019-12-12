e03c7c40c6b7b29f10ab396980bde6fc8e6f435f
Alexandra Servie <alexservie@Alexandras-MacBook-Pro-2.local>
2019-12-12 13:05:22 +0100

    again

---
+++ client/build/precache-manifest.4c1c32139c1d1f966aca1df5b59b6f57.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f1e99d2b1f533761e279295dca3eff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "466e36f18426b53cad21",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "363ee03a01b9f21045ac",
+    "url": "/static/css/main.407c0189.chunk.css"
+  },
+  {
+    "revision": "466e36f18426b53cad21",
+    "url": "/static/js/2.7153be3d.chunk.js"
+  },
+  {
+    "revision": "363ee03a01b9f21045ac",
+    "url": "/static/js/main.50817969.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "5cfd80fd560e31c6719e38f7def97aa9",
+    "url": "/static/media/apartment-5.5cfd80fd.jpg"
+  },
+  {
+    "revision": "c4e2f67a15ba997cba796072393a8485",
+    "url": "/static/media/icon-checkmark.c4e2f67a.png"
+  },
+  {
+    "revision": "b76d50ab6afc6a055e18185813d6bb01",
+    "url": "/static/media/icon-clock-edit.b76d50ab.png"
+  },
+  {
+    "revision": "7635289946fc7f9e294f8348e7cec077",
+    "url": "/static/media/icon-home.76352899.png"
+  },
+  {
+    "revision": "e5761e49ba8f2aa6946a9f2c3b1ae829",
+    "url": "/static/media/logo.e5761e49.png"
+  }
+]);
